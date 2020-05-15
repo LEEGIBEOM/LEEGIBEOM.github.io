1cc9c10058f2a7254c549f69ad9202c0e395a40b
rnrmf <rnrmf1gh@gmail.com>
2020-05-16 01:53:50 +0900

    Update Layout

---
+++ src/app.js
@@ -0,0 +1,4 @@
+const footer = document.querySelector(".footer"),
+  copyright = footer.querySelector(".footer__copyright");
+
+copyright.innerText += ` ${new Date().getFullYear()} LEEGIBEOM. All rights reserved.`;
